cf4d33424685ac284b0ba968abe56ebb6af63dba
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-25 21:46:46 -0300

    couto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um compilador?',
+    'Um compilador é um programa que traduz código fonte de uma linguagem de programação para linguagem de máquina.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O rio mais longo do mundo é o Nilo.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um banco de dados?',
+    'Um banco de dados é um sistema organizado para armazenar, gerenciar e recuperar informações.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz por favor em inglês?',
+    'Por favor em inglês é "Please".'
+)
